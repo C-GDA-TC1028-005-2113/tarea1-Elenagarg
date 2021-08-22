8ad04a197dd63f78f53bdd9655b46c3ff0da833e
Elenagarg <A01640861@itesm.mx>
2021-08-22 17:47:35 +0000

    Tarea 1

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,7 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-
-
+    base= float(input("Dame la base: "))
+    altura= float(input("Dame la altura: "))
+    a= (base*altura)/2
+    print ("El área es:", a)
+    
 
 if __name__ == '__main__':
     main()
